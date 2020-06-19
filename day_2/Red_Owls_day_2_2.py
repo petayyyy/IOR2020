ac018b1e8104b95b620f547534ceb7d522803d4c
petayyyy <47917455+petayyyy@users.noreply.github.com>
2020-06-19 10:34:00 +0300

    Update Red_Owls_day_2_2.py

--- day_2/Red_Owls_day_2_2.py
+++ day_2/Red_Owls_day_2_2.py
@@ -281,10 +281,6 @@ threading.Thread(target= main()).start()
 
 print navigate(x=0, y=0, z=1, speed=0.5, frame_id='body', auto_arm=True)
 rospy.sleep(3)
-
-print navigate(x=0, y=0, z=1, speed=0.5, frame_id='body', auto_arm=True)
-print('Захар делай скрин')
-rospy.sleep(6)
 print navigate(x=1, y=0.3, z=1.5, speed=0.5, yaw=math.radians(90),frame_id='aruco_map')
 print('Захар делай скрин')
 rospy.sleep(6)
